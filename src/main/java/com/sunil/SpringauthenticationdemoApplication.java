8c92fc279741ad81b7c2e8d797a7a9571057d078
sunildutt6 <sunildutt6@hotmail.com>
2023-03-03 17:13:20 +0100

    commit

--- src/main/java/com/sunil/SpringauthenticationdemoApplication.java
+++ src/main/java/com/sunil/SpringauthenticationdemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringsecuritypassworddemoApplication {
+public class SpringauthenticationdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecuritypassworddemoApplication.class, args);
+		SpringApplication.run(SpringauthenticationdemoApplication.class, args);
 	}
 
 }
